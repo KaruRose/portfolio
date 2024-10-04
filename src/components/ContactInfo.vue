b5e23852153d75f09728bf1a8ff452261f16cfd8
Carlos Rosas <rosasdavid60@gmail.com>
2024-10-04 14:53:06 -0400

    portfolio actualizado

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="contact-info">
+    <h5 class="text-h5 text-uppercase text-deep-purple-accent-1">{{ header }}</h5>
+    <div class="contact-info__item mt-3" v-for="(item, i) in data" :key="i">
+      <div class="contact-info__icon">
+        <v-icon size="x-large">{{ item.icon }}</v-icon>
+      </div>
+      <div class="contact-info__content">
+        <span class="text-uppercase text-button">{{ item.title }}</span>
+        <a :href="item.href" :target="item.target">{{ item.value }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+  name: 'ContactInfo',
+})
+
+defineProps({
+  header: String
+})
+
+const data = reactive([
+  // {
+  //   icon: 'mdi-cellphone-sound',
+  //   title: 'Phone',
+  //   value: '+ 58 414 3760 987',
+  //   href: 'tel:+584143760987',
+  //   target: null,
+  // },
+
+  {
+    icon: 'mdi-gmail',
+    title: 'Gmail',
+    value: 'rosasdavid60@gmail.com',
+    href: 'mailto:rosasdavid60@gmail.com',
+    target: null,
+  },
+  {
+    icon: 'mdi-gitlab',
+    title: 'GitLab',
+    value: '@KaruRose',
+    href: 'https://gitlab.com/KaruRose',
+    target: '_blank',
+  },
+  {
+    icon: 'mdi-github',
+    title: 'Github',
+    value: '@KaruRose',
+    href: 'https://github.com/KaruRose',
+    target: '_blank',
+  },
+ 
+])
+</script>
+
+<style scoped>
+.contact-info__item {
+  display: flex;
+  align-items: flex-end;
+}
+
+.contact-info__content {
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+}
+</style>
---
+++ src/components/Timelines.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="timeline mt-5">
+    <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Education & work</h5>
+    <v-timeline class="mt-5">
+      <v-timeline-item v-for="timeline in timelines" :key="timeline.id" :dot-color="timeline.color"
+                       :icon="timeline.icon" size="large">
+        <template v-slot:opposite>
+          <div :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
+               v-text="timeline.period"></div>
+        </template>
+        <div>
+          <h2 :class="`mt-n1 headline font-weight-light mb-1 text-${timeline.color}`">{{ timeline.title }}</h2>
+          <h3 :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`">{{ timeline.typeJob }}</h3>
+          <h5 :class="`text-button text-${timeline.color}`">{{ timeline.position }}</h5>
+          <span class="text-subtitle-2">{{ timeline.description }}</span>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+  name: 'Timelines'
+})
+
+const timelines = reactive([
+  {
+    id: 0,
+    icon: 'mdi-book-education',
+    color: 'cyan lighten-1',
+    title: 'Liceo Francisco Javier Pulgar',
+    period: 'MAR 2014 — SEP 2019',
+  },
+  {
+    id: 1,
+    icon: 'mdi-book-education',
+    color: 'blue-grey',
+    title: 'Universidad Territorial del estado Merida',
+    position: 'in progress',
+    period: '2024',
+  },
+  {
+    id: 2,
+    icon: 'mdi-briefcase-variant',
+    color: 'blue-darken-3',
+    title: 'RosasStore',
+    typeJob: 'Freelance',
+    position: 'front-end',
+    period: 'JUN 2024 — JUL 2024',
+    description: 'Ñº¡MI'
+  },
+  // {
+  //   id: 3,
+  //   icon: 'mdi-briefcase-variant',
+  //   color: 'blue-grey',
+  //   title: 'Moonion',
+  //   position: 'PHP Back-end developer',
+  //   period: 'JUN — SEP 2022',
+  //   description: 'API integration to e-commerce based on PHP yii2 framework'
+  // },
+  // {
+  //   id: 4,
+  //   icon: 'mdi-briefcase-variant',
+  //   color: 'deep-orange-lighten-1',
+  //   title: 'TuneLab',
+  //   position: 'Full-stack developer',
+  //   period: 'DEC 2022 — NOWADAYS',
+  //   description: 'Developing finTech application from scratch with Laravel 9, Vue.js 3 and Inertia.js'
+  // },
+])
+</script>
